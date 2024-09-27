50d9ffeb6bc4c3f9b9d24bb2bbb8d5ee7684f864
MursalinDevOps <mursalin1768@gmail.com>
2024-09-27 22:48:35 +0600

    problem solving task-2 completed

--- problem-solving.js
+++ problem-solving.js
@@ -1,5 +1,5 @@
 // TASK-1 : Make Even numbers using for loop and .map method
- 
+
 let oddNumbers = [1, 3, 5, 7, 9];
 let evenNumbers = [];
 
@@ -12,9 +12,18 @@ console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
 
 const numbers = [...oddNumbers];
 const evenIt = numbers.map((n) => {
-    if(n%2===1){
-        return n+1;
-    }
-})
+  if (n % 2 === 1) {
+    return n + 1;
+  }
+});
 console.log(evenIt);
 
+// TASK-2 : Get all the values that are divisible by 10 (using array.filter method)
+
+// array.filter method
+const arr = [33, 50, 79, 78, 90, 101, 30];
+const divisibleByTen = arr.filter((n) => n % 10 === 0);
+console.log(divisibleByTen); // [50, 90, 30]
+// array.find method
+const divisibleByTen2 = arr.find((n) => n % 10 === 0);
+console.log(divisibleByTen2); // [50]
